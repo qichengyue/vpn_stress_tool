afd2f0063194fe7e6e6fdaa18eb1606fd6c092ef
qicy-home-pc <qicy@home-pc.com>
2018-10-28 22:56:04 +0800

    Add code on test.py(some exam code for dtls..)

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import socket
+import threading
+
 from OpenSSL import SSL
 from OpenSSL._util import lib
+import os
 
 DTLSv1_METHOD = 7
 DTLSv12_METHOD = 8
@@ -12,26 +15,32 @@ SSL.Context._methods[DTLSv12_METHOD] = lib.DTLS_client_method
 ctx = SSL.Context(DTLSv12_METHOD)
 
 
-class vpn_dtls_socket(socket.socket):
-    def __init__(self, family=-1, type=-1, proto=-1, fileno=None):
-        super(vpn_dtls_socket, self).__init__(family, type, proto, fileno)
-        self.coreid = 0
+def dtls_proxy(target_ip, target_port, listen_ip, listen_port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((listen_ip, listen_port))
 
-    def set_coreid(self, coreid):
-        self.coreid = coreid
+    data, dtls_client_addr = server_socket.recvfrom(1500)
+    server_socket.sendto(data, (target_ip, target_port))
 
-    def sendto(self, data, flags=None, *args, **kwargs):
-        print('sendto invoked')
-        tmp_data = b'\x03' + data
-        super().sendto(b'abc', flags, *args, **kwargs)
+    while True:
+        data, addr = server_socket.recvfrom(1500)
+        if addr[0] == target_ip:
+            # The packet is from VPN server side, sendto VPN client side
+            server_socket.sendto(data, dtls_client_addr)
+        else:
+            # The packet is from VPN client side, send to VPN server side
+            server_socket.sendto(data, (target_ip, target_port))
 
 
-s = vpn_dtls_socket(socket.AF_INET, socket.SOCK_DGRAM)
+dtls_proxy_thread = threading.Thread(target=dtls_proxy, args=(host, 4567, '0.0.0.0', 8888))
+dtls_proxy_thread.start()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 con = SSL.Connection(ctx, s)
-con.connect((host, 4567))
+con.connect(('localhost', 8888))
 con.do_handshake()
 con.send(b'abc')
 
+print('Socket FD: %s' % s.fileno())
 print('Cipher:', con.get_cipher_name(), 'Protocol:', con.get_protocol_version_name())
-print('Receive:%s' %con.read(1024))
